ef01626e1deed1cd07f67644a4eb9ce64e3ec74c
bryanosaldomain <102651083+bryanosaldomain@users.noreply.github.com>
2024-06-27 14:41:21 +0800

    change bg color of webapp

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,21 @@ import TodoList from './components/todoList'
 
 function App() {
 
+  
+
 
   return (
     <div className='container mx-auto max-w-md h-screen py-6'>
-        <div className='flex flex-col p-4 border rounded-lg shadow-lg h-full bg-teal-900'>
+        <div className='flex flex-col p-4 rounded-lg shadow-lg h-full bg-teal-900 border-solid border-black border-4'>
           <div className='py-4'>
             <h1 className='text-2xl font-bold mb-4 text-center text-white '>
                 Bryan's Todo List
             </h1>
             <AddTodoForm/>
           </div>
-          <div className=''>
+            <div className='bg-black p-5 rounded-sm h-[100%] z-0'>
             <TodoList/>
-          </div>
+            </div>
         </div>
     </div>
   )
